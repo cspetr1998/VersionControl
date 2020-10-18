1a4a177d0f601bfae6818ee0509b3e8a3bb4f773
Csesznok Petronella <cspetra1998@gmail.com>
2020-10-18 21:10:55 +0200

    chart

--- 6gyakorlat/6gyakorlat/Form1.cs
+++ 6gyakorlat/6gyakorlat/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace _6gyakorlat
@@ -37,6 +38,7 @@ namespace _6gyakorlat
             xml.LoadXml(result);
 
             BindingList<RateData> Rates = new BindingList<RateData>();
+            dataGridView1.DataSource = Rates;
 
             foreach (XmlElement element in xml.DocumentElement)
             {
@@ -57,6 +59,22 @@ namespace _6gyakorlat
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
     }
