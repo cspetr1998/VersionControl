1ee1e65b76c5194790547ff13822b19b01f8a531
Csesznok Petronella <cspetra1998@gmail.com>
2020-10-25 13:50:35 +0100

    osztaly letrehozas

---
+++ 7gyakorlat/7gyakorlat/Entities/Gender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7gyakorlat.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+
+    // Példa enumerációs változók használatára
+    Gender ádámNeme = Gender.Male;
+    Gender évaNeme = (Gender)2;
+}
