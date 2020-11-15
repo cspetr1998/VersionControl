1a5da47862625137cb7ba0bc749146dcd8088927
Csesznok Petronella <cspetra1998@gmail.com>
2020-11-15 11:54:47 +0100

    car factory

---
+++ 8gyakorlat/8gyakorlat/Abstractions/ICarFactory.cs
@@ -0,0 +1,14 @@
+﻿using _8gyakorlat.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8gyakorlat.Abstractions
+{
+    public interface ICarFactory
+    {
+        Car CreateNew();
+    }
+}
