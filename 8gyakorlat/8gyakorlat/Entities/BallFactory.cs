cdb2201c9d97ab693910b804666dd4a49e7c5a0e
Csesznok Petronella <cspetra1998@gmail.com>
2020-11-15 11:48:22 +0100

    Absztrakció felhasználása a gyárakban

--- 8gyakorlat/8gyakorlat/Entities/BallFactory.cs
+++ 8gyakorlat/8gyakorlat/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _8gyakorlat.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,10 @@ using System.Windows.Forms;
 
 namespace _8gyakorlat.Entities
 {
-    public partial class BallFactory : Form
+    public partial class BallFactory : IToyFactory
+
     {
-        public Ball CreateNew()
+        public Toy CreateNew()
         {
             return new Ball();
         }
